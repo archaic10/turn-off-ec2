dcb9f1aed0b0acc0bfc8f5313e8cea1ac63759b4
Vitor França <vitor.franca.9400@gmail.com>
2024-05-07 08:54:47 -0300

    feat: create skeleton

---
+++ README.md
@@ -0,0 +1,12 @@
+# Turn off EC2
+ This action turn off an existing EC2 instance to AWS.
+
+### example
+```yml
+uses: archaic10/turn-off-ec2@main
+with:
+    access-key-id: ${{ secrets.ACESS_KEY_ID }}
+    secret-access-key: ${{ secrets.ACESS_SECRET_KEY }}
+    region: ${{ secrets.REGION }}
+    ami-instance: ${{ secrets.AMI_INSTANCE }}   
+```
---
+++ index.js
@@ -0,0 +1,59 @@
+const AWS = require('aws-sdk');
+const core = require('@actions/core')
+const accessKeyId = core.getInput('access-key-id');
+const secretAccessKey = core.getInput('secret-access-key');
+const region = core.getInput('region');
+const amiInstance = core.getInput('ami-instance');
+
+AWS.config.update({
+    accessKeyId: accessKeyId,
+    secretAccessKey: secretAccessKey,
+    region: region
+});
+
+const ec2 = new AWS.EC2();
+
+function stopInstance(instanceId) {
+    const params = {
+      InstanceIds: [instanceId]
+    };
+  
+    ec2.stopInstances(params, function(err, data) {
+      if (err){
+        console.log(err, err.stack);
+        return;
+      } 
+      
+      console.log('Instância desligada com sucesso:', data);
+    });
+}
+
+
+function validateParams(){
+    if(!accessKeyId && accessKeyId == ""){
+        core.setFailed("access-key-id undefined!");
+        return false;
+    }
+
+    if(!secretAccessKey && secretAccessKey == ""){
+        core.setFailed("secret-access-key undefined!");
+        return false;
+        
+    }
+
+    if(!region && region == ""){
+        core.setFailed("region undefined!");
+        return false;
+        
+    }
+
+    if(!amiInstance && amiInstance == ""){
+        core.setFailed("ami-instance undefined!");
+        return false;
+    }
+    return true;
+}
+
+if(validateParams()){
+    stopInstance(amiInstance);
+}
